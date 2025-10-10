7625acfa02e6514ea668d22dd0c0ca3b95a98af5
Kaiyun <kaiyunchan2012@hotmail.com>
2025-10-10 16:56:24 +0800

    Create TxtBox.jsx

---
+++ Demo/units/TxtBox.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function TxtBox({ name, value = '', onChange = null, extra = {} }) {
+    // init
+    const { type = "text", id = name, step, alias, validation, regex, placeholder, translation } = extra;
+    if(type === 'color' && value === '') {
+        value = '#585d93';
+    }
+    const [defaultValue, setValue] = useState(value);
+    const [passwordMode, setPasswordMode] = useState(true);
+    
+    // functions
+    function handleChange(e) {
+        setValue(e.target.value);
+        if (onChange) { onChange(e) };
+    };
+
+    function switchPwdMode() {
+        setPasswordMode(!passwordMode);
+    }
+
+    function changeColorCode(e) {
+        setValue(e.target.value);
+    }
+
+    // view
+    const commonProps = {
+        id,
+        name,
+        value: defaultValue,
+        onChange: handleChange,
+        autoComplete: 'off',
+        ...(placeholder && { placeholder }),
+        ...(step && { step }),
+        ...(validation && { "data-validation": validation }),
+        ...(regex && { "data-regex": regex }),
+        ...(translation && { "data-translation": translation }),
+        style: { display: 'block', width: '100%' }
+    };
+
+    return (
+        <>
+            {alias && <label htmlFor={id} className="name">{alias}</label>}
+            <div style={{ position: 'relative' }}>
+                <div className={`iweb-input iweb-input-${type === 'editor' ? 'textarea editor' : type}`}>
+                    {
+                        (type === 'textarea' || type === 'editor') ?
+                            (
+                                <textarea {...commonProps} className={type === 'editor' ? 'editor' : ''} />
+                            ) : 
+                            (
+                                <>
+                                    <input type={type === 'password' ? (passwordMode ? 'password' : 'text') : type} {...commonProps} />
+                                    { 
+                                        type === 'color' && 
+                                        ( 
+                                            <input type="text" value={defaultValue} maxLength={7} onChange={changeColorCode}/> 
+                                        ) 
+                                    }
+                                    {   
+                                        type === 'password' && 
+                                        (
+                                            <button type="button" className="switch-pwd-type" onClick={switchPwdMode}>
+                                                {passwordMode ? <FontAwesomeIcon icon="fa-eye-slash"/> : <FontAwesomeIcon icon="fa-eye"/>}
+                                            </button>
+                                        )
+                                    }
+                                </>
+                            )
+                    }
+                </div>
+            </div>
+        </>
+    );
+}
